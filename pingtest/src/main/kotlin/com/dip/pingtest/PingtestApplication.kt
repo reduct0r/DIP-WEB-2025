9af9160d8332d04115aaa631b85dab4f77adca6f
Timur <timurhay1@gmail.com>
2025-09-07 18:35:20 +0300

    start

---
+++ pingtest/src/main/kotlin/com/dip/pingtest/PingtestApplication.kt
@@ -0,0 +1,11 @@
+package com.dip.pingtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PingtestApplication
+
+fun main(args: Array<String>) {
+	runApplication<PingtestApplication>(*args)
+}
---
+++ pingtest/src/test/kotlin/com/dip/pingtest/PingtestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dip.pingtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PingtestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
