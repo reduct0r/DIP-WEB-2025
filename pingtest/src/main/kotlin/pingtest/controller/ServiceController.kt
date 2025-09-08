1582d113328366c841e08e6ef51c42aca300e0ed
Timur <timurhay1@gmail.com>
2025-09-08 04:16:44 +0300

    th start

--- pingtest/src/main/kotlin/pingtest/controller/ServiceController.kt
+++ pingtest/src/main/kotlin/pingtest/controller/ServiceController.kt
@@ -4,15 +4,23 @@ import com.dip.pingtest.service.ServiceService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class ServiceController(private val service: ServiceService) {
 
-    @GetMapping("/hello")
-    fun hello(model: Model): String {
+    @GetMapping("/services")
+    fun getServices(model: Model): String {
         val data = service.getWithTime()
-        model.addAttribute("time", data["time"])
-        model.addAttribute("services", data["services"])
-        return "test-index"
+//        model.addAttribute("time", data["time"])
+//        model.addAttribute("services", data["services"])
+        return "main/card"
+    }
+
+    @GetMapping("/service/{id}")
+    fun getService(@PathVariable id: Int, model: Model): String {
+        val service = service.getService(id)
+        model.addAttribute("service", service)
+        return "service"
     }
 }
