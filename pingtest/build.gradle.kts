fafe863dd7bf0dfa033ef913c4994151472328a9
Timur <timurhay1@gmail.com>
2025-10-21 01:17:17 +0300

    add redis and jwt conf

--- pingtest/build.gradle.kts
+++ pingtest/build.gradle.kts
@@ -34,6 +34,19 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("io.minio:minio:8.5.12")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
+    // Security JWT
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    // Redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
 }
 
 kotlin {
