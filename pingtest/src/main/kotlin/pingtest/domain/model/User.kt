2928dbf4e8d0e213bf65f9ef6a36019018383b22
Timur <timurhay1@gmail.com>
2025-10-21 02:25:39 +0300

    fix role

--- pingtest/src/main/kotlin/pingtest/domain/model/User.kt
+++ pingtest/src/main/kotlin/pingtest/domain/model/User.kt
@@ -2,6 +2,8 @@ package com.dip.pingtest.domain.model
 
 import com.dip.pingtest.domain.model.enums.Role
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -14,6 +16,7 @@ data class User(
     val id: Int = 0,
     var username: String,
     var password: String,
+    @Enumerated(EnumType.STRING)
     var role: Role = Role.USER,
     var preferences: String? = null
 )
